d5e64f15d81acc4be82d84a46ef99d6812985d1c
Ben Kolera <ben.kolera@gmail.com>
2013-10-16 15:14:05 +1000

    0.3.0 - withTransaction method

---
+++ src/main/scala/package.scala
@@ -0,0 +1,22 @@
+package com.benkolera
+
+import scalaz._
+import scala.slick.session.Session
+
+package object slick {
+  /*
+   * Because I really don't like how JDBC and Session.withTransaction works:
+   * - Autocommit should be true after the transaction just like begin & commit.
+   * - I hate how it forces me to throw exceptions to roll back.
+   */
+  def withTransaction[E,A]( session:Session )( f: => E \/ A ): E \/ A  = {
+    try {
+      session.withTransaction{
+        f.leftMap( e => { session.rollback ; e } )
+      }
+    } finally {
+      session.conn.setAutoCommit(true)
+    }
+  }
+
+}
